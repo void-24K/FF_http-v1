6998ac8d44c90ad8453ae5322e6470c8ad02842b
void-24K <78932083+void-24K@users.noreply.github.com>
2025-03-30 07:46:52 -0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,52 +1,125 @@
 {
-  "is_firewall_open" : false,
-  "billboard_bg_url" : "https:\/\/dl.cdn.freefiremobile.com\/common\/OB23\/version\/Patch_Bg.png",
-  "gop_url" : "",
-  "gamevar" : "var_name,comment,var_type,var_value,var_region,var_platform\nvar_name,comment,var_type,var_value,var_region,var_platform\nFastQuit_NativeDoneFast,FastQuit_NativeDoneFast,bool,true,,\nProcessModelInitAfterLoginInDeserialize,ProcessModelInitAfterLoginInDeserialize,bool,false,,\nEnableRecylePlayer,EnableRecylePlayer,bool,false,,\nEnableSlicedDestroyStremaerObject,EnableSlicedDestroyStremaerObject,bool,false,,\n",
-  "core_url" : "csoversea.castle.freefiremobile.com",
-  "login_download_optionalpack" : "optionalclothres:shaders|optionalpetres:optionalpetres_commonab_shader|optionallobbyres:",
-  "need_check_ip_list" : [
-    "202.81.108.9"
-  ],
-  "web_log_server" : "https:\/\/networkselftest.ff.garena.com\/api\/",
-  "remote_version" : "1.109.12",
-  "force_refresh_restype" : "optionalavatarres",
-  "max_video" : "",
-  "max_store" : "",
-  "garena_hint" : false,
-  "billboard_cdn_url" : "https:\/\/dl.dir.freefiremobile.com\/common\/OB48\/CSH\/patchupdate\/naenFjei38HE9bR01.ff_extend;https:\/\/dl.dir.freefiremobile.com\/common\/OB48\/CSH\/patchupdate\/naenFjei38HE9bR02.ff_extend;https:\/\/dl.dir.freefiremobile.com\/common\/OB48\/CSH\/patchupdate\/naenFjei38HE9bR03.ff_extend;https:\/\/dl.dir.freefiremobile.com\/common\/OB48\/CSH\/patchupdate\/naenFjei38HE9bR04.ff_extend;https:\/\/dl.dir.freefiremobile.com\/common\/OB48\/CSH\/patchupdate\/naenFjei38HE9bR05.ff_extend;https:\/\/dl.dir.freefiremobile.com\/common\/OB48\/CSH\/patchupdate\/naenFjei38HE9bR06.ff_extend",
-  "test_url" : "",
-  "client_ip" : "174.171.23.12",
-  "need_track_hotupdate" : true,
-  "max_web" : "",
-  "use_background_download" : false,
-  "cdn_url" : "https:\/\/dl.gmc.freefiremobile.com\/live\/ABHotUpdates\/",
-  "use_login_optional_download" : true,
-  "use_background_download_lobby" : false,
-  "login_failed_count" : 0,
-  "code" : 0,
-  "network_log_server" : "https:\/\/na.network.freefiremobile.com\/",
-  "appstore_url" : "https:\/\/itunes.apple.com\/app\/id1300146617",
-  "patchnote_url" : "https:\/\/dl.dir.freefiremobile.com\/common\/web_event\/aswqooiwd\/CK4GyNYn.html?lang=en",
-  "remote_option_version" : "optionallocres:0|optionalavatarres:537|optionalclothres:199999|optionalfootballres:27|optionalfullscreencgres:305|optionalhuntinggroundres:245|optionalinfection:125|optionalingameres:330|optionallobbyres:399|optionallonewolfres:71|optionallonewolfstrikeoutres:59|optionalludores:42|optionalmap1res:314|optionalmap2res:156|optionalmap4res:139|optionalmaphippores:115|optionalmapres:357|optionalnewblast:163|optionalpetres:686|optionalrushb:108|optionalrushingpetsres:84|optionalsnowduelres:35|optionalsocialres:166|optionaltrainingres:234|optionalugcres:624|optionalvoiceres:317|optionalwerewolves:153",
-  "garena_login" : false,
-  "latest_release_version" : "OB48",
-  "use_multithread_hash" : false,
-  "country_code" : "US",
-  "core_ip_list" : [
-    "0.0.0.0",
-    "50.109.27.134",
-    "129.226.2.163",
-    "129.226.1.13",
-    "129.226.1.16"
-  ],
-  "min_hint_size" : 1,
-  "is_review_server" : false,
-  "backup_cdn_url" : "https:\/\/dl.gmc.freefiremobile.com\/live\/ABHotUpdates\/",
-  "img_cdn_url" : "https:\/\/dl.gmc.freefiremobile.com\/common\/",
-  "is_server_open" : true,
-  "server_url" : "https:\/\/loginbp.ggblueshark.com\/",
-  "billboard_msg" : "",
-  "web_url" : "",
-  "gdpr_version" : 0
+  "log" : {
+    "entries" : [
+      {
+        "request" : {
+          "method" : "POST",
+          "bodySize" : 0,
+          "headersSize" : 1125,
+          "postData" : {
+            "mimeType" : "application\/x-www-form-urlencoded"
+          },
+          "cookies" : [
+
+          ],
+          "headers" : [
+            {
+              "name" : "Host",
+              "value" : "client.us.freefiremobile.com"
+            },
+            {
+              "name" : "X-Unity-Version",
+              "value" : "2018.4.11f1"
+            },
+            {
+              "name" : "Accept",
+              "value" : "*\/*"
+            },
+            {
+              "name" : "Authorization",
+              "value" : "Bearer eyJhbGciOiJIUzI1NiIsInN2ciI6IjIiLCJ0eXAiOiJKV1QifQ.eyJhY2NvdW50X2lkIjo5OTk5OTk5LCJuaWNrbmFtZSI6Ik5PZThiNWUwMjdkNyIsIm5vdGlfcmVnaW9uIjoiTkEiLCJsb2NrX3JlZ2lvbiI6Ik5BIiwiZXh0ZXJuYWxfaWQiOiJlOGI1ZTAyN2Q3YTJmYTM5YWE0Yzg4NzE2YWJlYTZlMiIsImV4dGVybmFsX3R5cGUiOjQsInBsYXRfaWQiOjAsImNsaWVudF92ZXJzaW9uIjoiMS4xMDkuMTIiLCJlbXVsYXRvcl9zY29yZSI6MCwiaXNfZW11bGF0b3IiOmZhbHNlLCJjb3VudHJ5X2NvZGUiOiJVUyIsImV4dGVybmFsX3VpZCI6MzgyNDE0NjM5NSwicmVnX2F2YXRhciI6MTAyMDAwMDA3LCJzb3VyY2UiOjIsImxvY2tfcmVnaW9uX3RpbWUiOjE3NDI1Nzc5NjAsImNsaWVudF90eXBlIjoxLCJzaWduYXR1cmVfbWQ1IjoiIiwidXNpbmdfdmVyc2lvbiI6MSwicmVsZWFzZV9jaGFubmVsIjoiaW9zIiwicmVsZWFzZV92ZXJzaW9uIjoiT0I0OCIsImV4cCI6MTc0MzM2NDU1M30.VmIVPA63Pyf5QmFFFWjOb7UWHqfozLIVgbYiNKHpEOg"
+            },
+            {
+              "name" : "ReleaseVersion",
+              "value" : "OB48"
+            },
+            {
+              "name" : "X-GA",
+              "value" : "v1 1"
+            },
+            {
+              "name" : "Accept-Encoding",
+              "value" : "gzip, deflate, br"
+            },
+            {
+              "name" : "Accept-Language",
+              "value" : "en-US,en;q=0.9"
+            },
+            {
+              "name" : "Content-Type",
+              "value" : "application\/x-www-form-urlencoded"
+            },
+            {
+              "name" : "Content-Length",
+              "value" : "16"
+            },
+            {
+              "name" : "User-Agent",
+              "value" : "Free%20Fire\/2019118071 CFNetwork\/1568.300.101 Darwin\/24.2.0"
+            },
+            {
+              "name" : "Connection",
+              "value" : "keep-alive"
+            }
+          ],
+          "queryString" : [
+
+          ],
+          "httpVersion" : "HTTP\/1.1",
+          "url" : "https:\/\/client.us.freefiremobile.com\/GetUnlockedFittingSlots"
+        },
+        "timings" : {
+          "connect" : 135.99991798400879,
+          "send" : 106.99987411499023,
+          "dns" : -1,
+          "ssl" : 99.999904632568359,
+          "wait" : 42.999982833862305,
+          "blocked" : -1,
+          "receive" : 6.0000419616699219
+        },
+        "serverIPAddress" : "148.153.132.72",
+        "response" : {
+          "status" : 200,
+          "statusText" : "OK",
+          "cookies" : [
+
+          ],
+          "headers" : [
+            {
+              "name" : "vary",
+              "value" : "Accept-Encoding"
+            },
+            {
+              "name" : "date",
+              "value" : "Sun, 30 Mar 2025 11:56:34 GMT"
+            },
+            {
+              "name" : "content-length",
+              "value" : "8"
+            },
+            {
+              "name" : "content-type",
+              "value" : "application\/octet-stream"
+            }
+          ],
+          "httpVersion" : "HTTP\/1.1",
+          "content" : {
+            "text" : "\n\u0002\b\u0001\n\u0002\b\u0002",
+            "size" : 8,
+            "mimeType" : "application\/octet-stream"
+          }
+        },
+        "time" : 52.999973297119141,
+        "startedDateTime" : "2025-03-30T06:56:34.329Z"
+      }
+    ],
+    "pages" : [
+
+    ],
+    "creator" : {
+      "name" : "Stream",
+      "version" : "1.0.6"
+    },
+    "version" : "1.2"
+  }
 }
